773f7fbc77e9530eed61587fb2397942167633cf
Sneilsen <87955167+Sneilsen@users.noreply.github.com>
2022-05-10 18:51:21 +0200

    Add files via upload
    login and register package.  need some debugging

--- server.js
+++ server.js
@@ -1,20 +1,92 @@
-require('dotenv').config()
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
 const express = require('express');
-const routes = require('./routes');
-const sequelize = require('./config/connection');
-
 const app = express();
-const PORT = process.env.PORT || 3001;
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const flash = require('express-flash');
+const session = require('express-session');
+const methodOverride = require('method-override');
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-// turn on routes
-app.use(routes);
+const initializePassport = require('./passport-config');
+initializePassport(
+    passport,
+     email => users.find(user => user.email === email),
+    id => users.find(user => user.id === id)
+)
 
-// turn on connection to db and server
-// if force is set to 'true' in sequelize.sync the tables recreate if there
-// is a new association created
-sequelize.sync({ force:  true }).then(() => {
-  app.listen(PORT, () => console.log('Now listening at port: ' + PORT));
-});
+const users = []
+
+app.set('view-engine', 'ejs')
+app.use(express.urlencoded({ extended: false }));
+app.use(flash());
+app.use(session({
+    
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    is_logged_in: false,
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(methodOverride('_method'))
+
+// middleware added to login and register //
+app.get('/', checkAuthenticated, (req, res) => {
+    res.render('index.ejs', { name: req.user.name }) 
+})
+
+app.get('/login', checkNotAuthenticated, (req, res) => {
+    res.render('login.ejs') 
+})
+
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: 'login',
+    failureFlash: true,
+}))
+
+app.get('/register', checkNotAuthenticated, (req, res) => {
+    res.render('register.ejs') 
+})
+
+app.post('/register', checkNotAuthenticated, (req, res) => {
+    try {
+        const hashedPassword = bcrypt.hash(req.body.password, 10)
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword,   
+         })
+         res.redirect('/login')
+    } catch {
+        res.redirect('/register')
+    }
+    console.log(users)
+})
+
+app.delete('/logout', (req, res) => {
+    req.logOut()
+    res.redirect('/login')
+})
+
+//  middleware for logging in //
+
+function checkAuthenticated(req, res, next) {
+    if (req.checkAuthenticated()) {
+        return next()
+    }
+
+    res.redirect('/login')
+}
+function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return res.redirect('/')
+    }
+    next()
+}
+app.listen(3006)
