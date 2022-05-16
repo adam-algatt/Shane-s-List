a393655f7b15be608e28334634dfa7082262fb8d
Rick Barendse <dazedeyes@yahoo.com>
2022-05-16 15:09:37 -0700

    view single post.  Leave comment.

--- routes/home-routes.js
+++ routes/home-routes.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Post, Category, User, 
-    //Comment, Vote 
+const { Post, Category, User, Comment, //Vote 
 } = require('../models');
 
 router.get('/', (req, res) => {
@@ -42,52 +41,52 @@ router.get('/login', (req, res) => {
     res.render('login');
 });
 
-// router.get('/post/:id', (req, res) => {
-//     Post.findOne({
-//         where: {
-//             id: req.params.id
-//         },
-//         attributes: [
-//             'id',
-//             'post_url',
-//             'title',
-//             'created_at',
-//             [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-//         ],
-//         include: [
-//             {
-//                 model: Comment,
-//                 attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-//                 include: {
-//                     model: User,
-//                     attributes: ['username']
-//                 }
-//             },
-//             {
-//                 model: User,
-//                 attributes: ['username']
-//             }
-//         ]
-//     })
-//         .then(dbPostData => {
-//             if (!dbPostData) {
-//                 res.status(404).json({ message: 'No post found with this is' });
-//                 return;
-//             }
+router.get('/post/:post_id', (req, res) => {
+    Post.findOne({
+        where: {
+            post_id: req.params.post_id
+        },
+        attributes: [
+            'post_id',
+            'title',
+            'description',
+            'product_category'
+            //[sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['comment_id', 'comment_text', 'comment_user_id'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'No post found with this is' });
+                return;
+            }
 
-//             // serialize data 
-//             const post = dbPostData.get({ plain: true });
+            // serialize data 
+            const post = dbPostData.get({ plain: true });
             
-//             // pass data to template
-//             res.render('single-post', { 
-//                 post,
-//                 loggedIn: req.session.loggedIn 
-//             });
-//         })    
-//         .catch(err => {
-//             console.log(err);
-//             res.status(500).json(err);
-//     });
-// });
+            // pass data to template
+            res.render('single-post', { 
+                post,
+            //    loggedIn: req.session.loggedIn 
+            });
+        })    
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+    });
+});
 
 module.exports = router;
