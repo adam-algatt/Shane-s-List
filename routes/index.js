cbcba5b8a5407d8a690fa12506a975122c570348
algattAdam <98243574+adam-algatt@users.noreply.github.com>
2022-05-22 17:41:55 -0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,15 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes.js');
 const dashboardRoutes = require('./dashboard-routes.js');
-const singlePostRoutes = require('./single-post-routes.js');
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
-router.use('/api/posts/:post_id', singlePostRoutes);
 
 router.use((req, res) => {
     res.status(404).end();
 });
 
 module.exports = router;
+
 
